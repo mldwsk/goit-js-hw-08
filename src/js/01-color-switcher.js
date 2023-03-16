a1aecc6c49a2c9ba2297e5192824ba37b9cbb416
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-03-16 22:14:22 +0100

    Solved homework 8 from module 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function colorChanger () {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timeout = setTimeout(colorChanger, 1000);
+}
+
+function startColors() {
+  if (startButton.className === 'enabled') {
+    startButton.className = 'disabled';
+    stopButton.className = 'enabled';
+    colorChanger();
+  }
+}
+
+function stopColors() {
+  if (stopButton.className === 'enabled') {
+    startButton.className = 'enabled';
+    stopButton.className = 'disabled';
+    clearTimeout(timeout);
+  }
+}
+
+let timeout;
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+startButton.className = 'enabled';
+stopButton.className = 'disabled';
+startButton.addEventListener('click', startColors);
+stopButton.addEventListener('click', stopColors);
